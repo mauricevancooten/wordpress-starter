1b0a64a84664c2328d2e4921bada0cbbd9ac1141
Maurice van Cooten <studio@kosmo.co.nz>
2015-01-11 14:44:11 +1300

    a basic starting template for WP theming

---
+++ starting-template/footer.php
@@ -0,0 +1,11 @@
+<footer>
+
+	<p><small>Website by the Author</small></p>
+    
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ starting-template/index.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<div id="wrapper">
+
+	<div id="container">
+    
+   	<?php query_posts('showposts=&order=ASC'); ?>
+
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+      	<article>
+
+			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" /><?php the_title(); ?></a></h2>
+				
+			<?php the_content(); ?>
+
+		</article>
+			
+<?php endwhile; else: ?>
+
+<p><?php_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+<?php endif; ?>
+
+	</div> <!-- #container -->
+
+</div> <!-- #wrapper -->
+
+
+<?php get_footer(); ?>
