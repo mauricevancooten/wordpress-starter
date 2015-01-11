adcd6936aa062b430a81199986baa20e4323c041
Maurice van Cooten <studio@kosmo.co.nz>
2015-01-11 15:42:46 +1300

    functions for native menu, sidebar, read me replacement text, etc.

---
+++ articles-template/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+// Register menu
+
+register_nav_menus(
+	array(
+	'main-menu' => 'Main Menu'
+	)
+);
+
+// Register aside sidebar
+
+$aside_sidebar = array(
+    'name'          => 'Aside',
+    'id'            => 'aside',
+    'description'   => 'Widgets placed here will go on the right hand side bar',
+    'class'         => '',
+    'before_widget' => '<div>',
+    'after_widget'  => "</div>\n",
+    'before_title'  => '<h3>',
+    'after_title'   => "</h3>\n",
+);
+
+register_sidebar( $aside_sidebar );
+
+// Thumbnails
+
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 380, 380, true );
+
+// Replaces the excerpt "more" text by a link
+
+function new_excerpt_more($more) {
+       global $post;
+    return '<a class="moretag" title="'. get_the_title($post->ID) . '" href="'. get_permalink($post->ID) . '"> Read more...</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
