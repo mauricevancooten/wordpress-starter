136a88a99dc3de77bdcbb9519e3be1012aaa323b
Maurice van Cooten <studio@kosmo.co.nz>
2015-01-11 15:04:40 +1300

    updated CSS url to include them name

--- starting-template/header.php
+++ starting-template/header.php
@@ -12,7 +12,7 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
-  <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/insightful/public/assets/css/main.css" >
+  <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/starting-template/public/assets/css/styles.css" >
 
 </head>
 
@@ -21,7 +21,7 @@
   
 
   <header>
-    <h1><?php wp_title() ?></h1>
+    <h1><?php the_title() ?></h1>
   </header> 
 
   <nav>
@@ -33,6 +33,3 @@
     </ul>
 
   </nav>
-
-  
-
