50402c1f8db354bc3fc985ac851ebf9d070382fb
Maurice van Cooten <maurice@minus.nz>
2015-10-27 07:00:05 +1300

    new commerce template

---
+++ commerce-template/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// Disable default stylesheet.
+
+define('WOOCOMMERCE_USE_CSS', false);
+
+// Theme support for Woocommerce
+
+add_theme_support( 'woocommerce' );
---
+++ commerce-template/woocommerce.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="wrapper">
+
+	<div class="container">
+
+		
+
+				<?php woocommerce_content(); ?>
+
+			
+
+	</div> <!-- .container -->
+
+</div> <!-- .wrapper -->
+
+<?php get_footer(); ?>
