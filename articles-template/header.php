1dda6baa26b2a91a1a0e56ae7a3e0dbf4a91c331
Maurice van Cooten <maurice@minus.nz>
2016-03-24 15:57:35 +1300

    more accurate link to stylesheet and updated nav to simpler version

--- articles-template/header.php
+++ articles-template/header.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php wp_title() ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_url' ); ?>/css/styles.css">
+    <?php wp_head(); ?> 
+  </head>
 
-<head>
-  
-  <meta charset="utf-8">
-  <title><?php wp_title() ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/articles-template/assets/css/styles.css" >
+  <body>
 
-  <?php wp_head(); ?> 
-   
-</head>
+    <header>
 
-<body>
+      <h1><?php echo get_bloginfo( 'name' ); ?></h1>
+      
+    </header> 
 
-  <header>
-
-    <h1><?php echo get_bloginfo( 'name' ); ?></h1>
-    
-  </header> 
-
-  <?php
-      $main_menu = array(
-      'theme_location' => 'main-menu',
-      'container' => '',
-      'items_wrap' => '<ul>%3$s</ul>'
-      );
-  ?>
-
-  <nav><?php wp_nav_menu($main_menu); ?></nav>
+      <nav class="site-navigation">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'main-menu', 
+            'container' => false,
+            'menu_id' => false,
+            'items_wrap' => '<ul>%3$s</ul>'
+            )
+          ); ?>
+      </nav>
