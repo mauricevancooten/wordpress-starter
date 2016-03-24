a33ff0fa81ae2cf096ea30f578d07b9ecb558169
Maurice van Cooten <maurice@minus.nz>
2016-03-24 15:56:55 +1300

    more accurate link to stylesheet

--- starting-template/header.php
+++ starting-template/header.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php wp_title() ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_url' ); ?>/css/styles.css">
+    <?php wp_head(); ?> 
+  </head>
 
-<head>
-  
-  <meta charset="utf-8">
-  <title><?php wp_title() ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_url' ); ?>/assets/css/styles.css" >
-  
-  <?php wp_head(); ?> 
+  <body>
 
-</head>
+    <header>
 
-<body>
-
-  <header>
-
-    <h1><?php echo get_bloginfo( 'name' ); ?></h1>
-    
-  </header> 
-
-  <nav>
+      <h1><?php echo get_bloginfo( 'name' ); ?></h1>
       
-    <ul>
-      <li><a href="<?php echo site_url(); ?>">Home</a></li>
-      <li><a href="<?php echo site_url(); ?>/sample-page">Sample Page</a></li>
-      <li><a href="<?php echo site_url(); ?>/contact">Contact</a></li>
-    </ul>
+    </header> 
 
-  </nav>
+    <nav>
+        
+      <ul>
+        <li><a href="<?php echo site_url(); ?>">Home</a></li>
+        <li><a href="<?php echo site_url(); ?>/sample-page">Sample Page</a></li>
+        <li><a href="<?php echo site_url(); ?>/contact">Contact</a></li>
+      </ul>
+
+    </nav>
