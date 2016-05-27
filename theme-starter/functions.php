10581effae5e9fdd6d4575a7fcdb0d5936ddad9c
Maurice van Cooten <maurice@minus.nz>
2016-05-27 12:02:33 +1200

    customised theme name and changed config

--- theme-starter/functions.php
+++ theme-starter/functions.php
@@ -2,10 +2,10 @@
 
 // Enqueue stylesheet
 
-function wp_articles_enqueue_style() {
-    wp_enqueue_style( 'wp-articles-style', get_stylesheet_uri() ); 
+function minus_enqueue_style() {
+    wp_enqueue_style( 'minus-style', get_stylesheet_uri() ); 
 }
-add_action( 'wp_enqueue_scripts', 'wp_articles_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'minus_enqueue_style' );
 
 // Register menu
 
